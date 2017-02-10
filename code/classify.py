381a23a15c947aafa14fbe3ba4e33b3a319a5aa8
avn3r <abnera>
2017-02-10 00:05:09 -0800

    CNN Model v2: Both models now use Xception

--- code/classify.py
+++ code/classify.py
@@ -1,4 +1,4 @@
-'''
+"""
 Author: Abner Ayala-Acevedo
 
 Dataset: Kaggle Dataset Dogs vs Cats
@@ -16,11 +16,13 @@ test_dir/
         ...
 
 If you need using a multi-class classification model change binary_cross_entropy to categorical_cross_entropy
-'''
+"""
 
 import sys
 import os
 import csv
+import glob
+import shutil
 import numpy as np
 import tensorflow as tf
 from keras.utils.np_utils import probas_to_classes
@@ -34,60 +36,57 @@ np.random.seed(seed=seed)
 tf.set_random_seed(seed=seed)
 
 # parameters dependent on your dataset: modified to your example
-nb_test_samples = 12500  # Total number of test samples. change based on your data
-img_width, img_height = 512, 512  # must match the fix size of your train image sizes. 600, 150 for text_images
+img_width, img_height = 299, 299  # must match the fix size of your train image sizes. 600, 150 for text_images
 batch_size = 32  # try 4, 8, 16, 32, 64, 128, 256 dependent on CPU/GPU memory capacity (powers of 2 values).
 
 # default paths
 model_name = 'model.json'
-model_weights = 'model_weights.h5'
+model_weights = 'top_model_weights.h5'
 results_name = 'predictions.csv'
 
 
 def classify(trained_model_dir, test_data_dir, results_path):
     # load json and create model
-    json_file = open(trained_model_dir + model_name, 'r')
+    json_file = open(os.path.join(trained_model_dir, model_name), 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = model_from_json(loaded_model_json)
-    model.load_weights(trained_model_dir + model_weights)
+    model.load_weights(os.path.join(trained_model_dir, model_weights))
 
-    # Read Data and Augment it: Make sure to select augmentations that are appropriate to your images.
-    # To save augmentations un-comment save lines and add to your flow parameters.
+    # Read Data
     test_datagen = ImageDataGenerator(rescale=1. / 255)
-
-    # with open("results/dogs_cats/predictions.csv", "w") as file:
-    #     csv_writer = csv.writer(file)
-    #     csv_writer.writerow(('id', 'labels'))
-    #     for _, _, images in os.walk(test_data_dir):
-    #         for im in images:
-    #             pic_id = im.split(".")[0]
-    #             img = load_img(test_data_dir + im)
-    #             img = imresize(img, size=(img_height, img_width))
-    #             test_x = img_to_array(img).reshape(img_height, img_width, 3)
-    #             test_x = test_x.reshape((1,) + test_x.shape)
-    #             test_generator = test_datagen.flow(test_x,
-    #                                                batch_size=1,
-    #                                                shuffle=False)
-    #             prediction = model.predict_generator(test_generator, 1)[0][0]
-    #             csv_writer.writerow((pic_id, prediction))
-
     test_generator = test_datagen.flow_from_directory(test_data_dir,
                                                       target_size=(img_width, img_height),
                                                       batch_size=batch_size,
                                                       shuffle=False)
-    # fine-tune the model
+
+    # Calculate class posteriors probabilities
     y_probabilities = model.predict_generator(test_generator,
-                                          val_samples=test_generator.nb_sample)
+                                              val_samples=test_generator.nb_sample)
+    # Calculate class labels
     y_classes = probas_to_classes(y_probabilities)
-    ids = [x.split("/")[1].split('.')[0] for x in test_generator.filenames]
+    filenames = [filename.split('/')[1] for filename in test_generator.filenames]
+    ids = [filename.split('.')[0] for filename in filenames]
 
-    with open(results_path + results_name, 'w') as file:
+    # save results as a csv file in the specified results directory
+    with open(os.path.join(results_path, results_name), 'w') as file:
         writer = csv.writer(file)
-        writer.writerow(('id', 'class1_prob', 'label'))
-        writer.writerows(zip(ids, y_probabilities[:, 1], y_classes))
+        writer.writerow(('id', 'class0_prob', 'class1_prob', 'label'))
+        writer.writerows(zip(ids, y_probabilities[:, 0], y_probabilities[:, 1], y_classes))
 
-    print(y_classes)
+    # # semi-supervise learning: save the classified test data in their respective train class folder to increase
+    # # your training dataset to include training
+    # validation_data_dir = os.path.join(test_data_dir, '../validation')
+    # validation_datagen = ImageDataGenerator(rescale=1. / 255)
+    # validation_generator = validation_datagen.flow_from_directory(validation_data_dir,
+    #                                                               target_size=(img_width, img_height),
+    #                                                               batch_size=batch_size,
+    #                                                               shuffle=False)
+    # labels_dictionary = dict((value, key) for key, value in validation_generator.class_indices.items())
+    # for i, filename in enumerate(glob.glob(os.path.join(test_data_dir, 'test', '*.*'))):
+    #     if y_probabilities[i, y_classes[i]] > .9995:
+    #         shutil.copy(filename, os.path.join(test_data_dir, '../train', labels_dictionary.get(y_classes[i])))
+
 
 if __name__ == '__main__':
     if not len(sys.argv) == 4:
@@ -95,9 +94,10 @@ if __name__ == '__main__':
         print('Example: python code/classify.py model/dogs_cats data/dogs_cats/test/ results/dogs_cats/')
         sys.exit(2)
     else:
-        model_dir = sys.argv[1]
-        test_dir = sys.argv[2]
-        results_dir = sys.argv[3]
+        model_dir = os.path.abspath(sys.argv[1])
+        test_dir = os.path.abspath(sys.argv[2])
+        results_dir = os.path.abspath(sys.argv[3])
+        os.makedirs(results_dir, exist_ok=True)
 
     classify(model_dir, test_dir, results_dir)  # train model
 
--- code/fine_tune.py
+++ code/fine_tune.py
@@ -1,4 +1,4 @@
-'''
+"""
 Author: Abner Ayala-Acevedo
 
 This script based on examples provided in the keras documentation and a blog.
@@ -36,116 +36,111 @@ data/
 Example has 1000 training examples for each class, and 400 validation examples for each class.
 The data folder already contains the dogs vs cat data you simply need to run script. For the dogs_cats classification
 you can find a model already trained in the model folder. Feel free to create your own data.
-'''
+"""
 
 import sys
-import numpy as np
-import tensorflow as tf
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import GlobalAveragePooling2D
-from keras.layers import Dense, BatchNormalization, Dropout
-from keras.optimizers import SGD
+import os
+from keras.layers import *
+from keras.optimizers import *
 from keras.applications import *
+from keras.models import Model
+from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras import backend as k
-from keras.models import Model
 
 # fix seed for reproducible results (only works on CPU, not GPU)
 seed = 9
 np.random.seed(seed=seed)
 tf.set_random_seed(seed=seed)
 
-# parameters dependent on your dataset: modified to your example
-nb_train_samples = 2000  # Total number of train samples. NOT including augmented images
-nb_validation_samples = 800  # Total number of train samples. NOT including augmented images.
-nb_classes = 2  # number of classes
-img_width, img_height = 512, 512  # change based on the shape/structure of your images
-
 # hyper parameters for model
-based_model_last_block_layer_number = 172  # value is based on based model selected.
+nb_classes = 2  # number of classes
+based_model_last_block_layer_number = 126  # value is based on based model selected.
+img_width, img_height = 299, 299  # change based on the shape/structure of your images
 batch_size = 32  # try 4, 8, 16, 32, 64, 128, 256 dependent on CPU/GPU memory capacity (powers of 2 values).
 nb_epoch = 50  # number of iteration the algorithm gets trained.
 learn_rate = 1e-4  # sgd learning rate
 momentum = .9  # sgd momentum to avoid local minimum
+transformation_ratio = .05  # how aggressive will be the data augmentation/transformation
 
 
 def train(train_data_dir, validation_data_dir, model_path):
     # Pre-Trained CNN Model using imagenet dataset for pre-trained weights
-    base_model = InceptionV3(input_shape=(img_width, img_height, 3), weights='imagenet', include_top=False)
+    base_model = Xception(input_shape=(img_width, img_height, 3), weights='imagenet', include_top=False)
 
     # Top Model Block
     x = base_model.output
-    x = BatchNormalization(axis=3)(x)
     x = GlobalAveragePooling2D()(x)
-    x = Dense(1024, activation='relu')(x)
-    x = Dropout(.5)(x)
     predictions = Dense(nb_classes, activation='softmax')(x)
 
     # add your top layer block to your base model
     model = Model(base_model.input, predictions)
+    print(model.summary())
 
-    # let's visualize layer names and layer indices to see how many layers
-    # we should freeze: this is used to define based_model_last_block_layer_number
-    for i, layer in enumerate(model.layers):  # comment these two lines once the correct based_model_last_block_layer
-        print(i, layer.name)  # has been selected
+    # # let's visualize layer names and layer indices to see how many layers/blocks to re-train
+    # # uncomment when choosing based_model_last_block_layer
+    # for i, layer in enumerate(model.layers):
+    #     print(i, layer.name)
 
     # first: train only the top layers (which were randomly initialized)
-    # i.e. freeze all convolution layers
+    # i.e. freeze all layers of the based model that is already pre-trained.
     for layer in base_model.layers:
         layer.trainable = False
 
     # Read Data and Augment it: Make sure to select augmentations that are appropriate to your images.
     # To save augmentations un-comment save lines and add to your flow parameters.
     train_datagen = ImageDataGenerator(rescale=1. / 255,
-                                       rotation_range=20,
-                                       shear_range=.2,
-                                       zoom_range=.2,
-                                       cval=.2,
+                                       rotation_range=transformation_ratio,
+                                       shear_range=transformation_ratio,
+                                       zoom_range=transformation_ratio,
+                                       cval=transformation_ratio,
                                        horizontal_flip=True,
                                        vertical_flip=True)
 
     validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
+    os.makedirs(os.path.join(os.path.abspath(train_data_dir), '../preview'), exist_ok=True)
     train_generator = train_datagen.flow_from_directory(train_data_dir,
-                                                        target_size=(img_height, img_width),
+                                                        target_size=(img_width, img_height),
                                                         batch_size=batch_size,
                                                         class_mode='categorical')
-                                                        # save_to_dir=data_dir + '/preview',
-                                                        # save_prefix='aug',
-                                                        # save_format='jpeg')
+    # save_to_dir=os.path.join(os.path.abspath(train_data_dir), '../preview')
+    # save_prefix='aug',
+    # save_format='jpeg')
     # use the above 3 commented lines if you want to save and look at how the data augmentations look like
 
     validation_generator = validation_datagen.flow_from_directory(validation_data_dir,
-                                                            target_size=(img_height, img_width),
-                                                            batch_size=batch_size,
-                                                            class_mode='categorical')
+                                                                  target_size=(img_width, img_height),
+                                                                  batch_size=batch_size,
+                                                                  class_mode='categorical')
 
     model.compile(optimizer='nadam',
                   loss='categorical_crossentropy',  # categorical_crossentropy if multi-class classifier
                   metrics=['accuracy'])
 
     # save weights of best training epoch: monitor either val_loss or val_acc
-    top_weights_path = model_path + 'top_model_weights.h5'
+
+    top_weights_path = os.path.join(os.path.abspath(model_path), 'top_model_weights.h5')
     callbacks_list = [
-        ModelCheckpoint(top_weights_path, monitor='val_acc', verbose=1, save_best_only=True, mode='auto'),
-        EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
+        ModelCheckpoint(top_weights_path, monitor='val_acc', verbose=1, save_best_only=True),
+        EarlyStopping(monitor='val_acc', patience=5, verbose=0)
     ]
 
     # Train Simple CNN
     model.fit_generator(train_generator,
-                        samples_per_epoch=nb_train_samples,
+                        samples_per_epoch=train_generator.nb_sample,
                         nb_epoch=nb_epoch / 5,
                         validation_data=validation_generator,
-                        nb_val_samples=nb_validation_samples,
+                        nb_val_samples=validation_generator.nb_sample,
                         callbacks=callbacks_list)
+
+    # verbose
+    print("\nStarting to Fine Tune Model\n")
 
     # add the best weights from the train top model
     # at this point we have the pre-train weights of the base model and the trained weight of the new/added top model
     # we re-load model weights to ensure the best epoch is selected and not the last one.
     model.load_weights(top_weights_path)
-
-    # verbose
-    print("\nStarting to Fine Tune Model\n")
 
     # based_model_last_block_layer_number points to the layer in your model you want to train.
     # For example if you want to train the last block of a 19 layer VGG16 model this should be 15
@@ -159,28 +154,28 @@ def train(train_data_dir, validation_data_dir, model_path):
 
     # compile the model with a SGD/momentum optimizer
     # and a very slow learning rate.
-    model.compile(optimizer=SGD(lr=learn_rate, momentum=momentum),
+    model.compile(optimizer='nadam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     # save weights of best training epoch: monitor either val_loss or val_acc
-    final_weights_path = model_path + 'model_weights.h5'
+    final_weights_path = os.path.join(os.path.abspath(model_path), 'model_weights.h5')
     callbacks_list = [
-        ModelCheckpoint(final_weights_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min'),
-        EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
+        ModelCheckpoint(final_weights_path, monitor='val_acc', verbose=1, save_best_only=True),
+        EarlyStopping(monitor='val_loss', patience=5, verbose=0)
     ]
 
     # fine-tune the model
     model.fit_generator(train_generator,
-                        samples_per_epoch=nb_train_samples,
+                        samples_per_epoch=train_generator.nb_sample,
                         nb_epoch=nb_epoch,
                         validation_data=validation_generator,
-                        nb_val_samples=nb_validation_samples,
+                        nb_val_samples=validation_generator.nb_sample,
                         callbacks=callbacks_list)
 
     # save model
     model_json = model.to_json()
-    with open(model_path + 'model.json', 'w') as json_file:
+    with open(os.path.join(os.path.abspath(model_path), 'model.json'), 'w') as json_file:
         json_file.write(model_json)
 
 
@@ -190,11 +185,14 @@ if __name__ == '__main__':
         print('Example: python code/fine_tune.py data/dogs_cats/ model/dog_cats/')
         sys.exit(2)
     else:
-        data_dir = sys.argv[1]
-        train_dir = data_dir + '/train'  # change to your train path. Inside, each class should have it's own folder
-        validation_dir = data_dir + '/validation'  # validation path. Inside, each class should have it's own folder
-        model_dir = sys.argv[2]
-        
+        data_dir = os.path.abspath(sys.argv[1])
+        train_dir = os.path.join(os.path.abspath(data_dir), 'train')  # Inside, each class should have it's own folder
+        validation_dir = os.path.join(os.path.abspath(data_dir), 'validation')  # each class should have it's own folder
+        model_dir = os.path.abspath(sys.argv[2])
+
+        os.makedirs(os.path.join(os.path.abspath(data_dir), 'preview'), exist_ok=True)
+        os.makedirs(model_dir, exist_ok=True)
+
     train(train_dir, validation_dir, model_dir)  # train model
 
     # release memory
